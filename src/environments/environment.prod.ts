06bf7128b5bece7f825652a6600ce9b82927f7a3
paradigim <pritamdas71.pd@gmail.com>
2021-01-22 01:09:15 +0530

    firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBpX2XZM4YbN1XL5emw07LzAG5i0PpanPE",
+    authDomain: "blog-9a5ab.firebaseapp.com",
+    databaseURL: "https://blog-9a5ab.firebaseio.com",
+    projectId: "blog-9a5ab",
+    storageBucket: "blog-9a5ab.appspot.com",
+    messagingSenderId: "97460591548",
+    appId: "1:97460591548:web:0382f5da7008cabd9969b9",
+    measurementId: "G-PRYCPK52S2"
+  }
 };
