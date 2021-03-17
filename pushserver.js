ceccf892a78fbc9485be3a0d9765643401b5bf0b
paradigim <pritamdas71.pd@gmail.com>
2021-03-17 09:00:00 +0530

    changes

--- pushserver.js
+++ pushserver.js
@@ -1,22 +1,18 @@
 
 const webpush = require('web-push');
 const express = require('express');
-
 const vapidKeys = {
     "publicKey":"BF7ekuEOKJrtvX4ornpRrZkkv_ALrNVb4r5RzeqzOgZP-oorGGxQsUROVK2oTymCDEkQKlaNb2WVYplrrtp9MtE",
     "privateKey":"M9B2G_q8S6QaN_QwOhshyNa-4tvhm4KzsSrNF51ZNGg"
 };
+
+const app = express();
 
 webpush.setVapidDetails(
     'mailto:example@yourdomain.org',
     vapidKeys.publicKey,
     vapidKeys.privateKey
 );
-
-const app = express();
-
-const port = process.env.PORT || 8082;
-app.listen(port)
 
 app.post('/subscribe', (req, res) => {
     const subscription = req.body;
@@ -48,3 +44,5 @@ app.post('/subscribe', (req, res) => {
         })
     )
 })
+
+app.listen(process.env.PORT || 8082);
