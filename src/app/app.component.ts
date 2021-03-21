e772e87eb07ffb55e1c23d6a59516aadceb43e57
paradigim <pritamdas71.pd@gmail.com>
2021-03-21 18:15:34 +0530

    pwa implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SwPush, SwUpdate } from '@angular/service-worker';
 import { PushNotification } from './services/push-notification.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,17 +10,49 @@ import { PushNotification } from './services/push-notification.service';
 
 export class AppComponent {
 
+  defferedPrompt;
+  subscriptionObj: any;
+
   VAPID_PUBLIC_KEY = "BF7ekuEOKJrtvX4ornpRrZkkv_ALrNVb4r5RzeqzOgZP-oorGGxQsUROVK2oTymCDEkQKlaNb2WVYplrrtp9MtE";
 
   constructor(
     private swPush: SwPush,
     private swUpdate: SwUpdate,
     private pushNotificationService: PushNotification
-  ) { }
+  ) {}
 
   ngOnInit() {
+    // window.addEventListener('beforeinstallprompt', (e) => {
+    //   e.preventDefault();
+    //   this.defferedPrompt = e;
+    //   this.defferedPrompt.prompt();
+
+    //   this.defferedPrompt.userChoice.then(choice => {
+    //     if (choice.outcome === 'accepted') {
+    //       console.log('Accepted');
+    //     }
+    //     this.defferedPrompt = null;
+    //   })
+    // })
+    // this.updateApp();
+    this.subscribeToNotifications();
     this.getPushNotificationMsg();
+    this.pushActionOnClick();
   }
+
+  pushActionOnClick() {
+    this.swPush.notificationClicks.subscribe(payload => {
+      window.open(payload.notification.data.url);
+    })
+  }
+
+  // updateApp() {
+  //   this.swUpdate.available.subscribe(() => {
+  //     this.swUpdate.activateUpdate().then(() => {
+  //       document.location.reload();
+  //     })
+  //   })
+  // }
 
   getPushNotificationMsg() {
     this.swPush.messages.subscribe(data => {
@@ -36,16 +69,14 @@ export class AppComponent {
       })
       .then(sub => {
         console.log('SUB: ', sub);
-        this.pushNotificationService.addPushSubscriber(sub).subscribe()
+        this.subscriptionObj = sub;
+        // this.pushNotificationService.addPushSubscriber(sub).subscribe()
       })
       .catch(err => console.error("Could not subscribe to notifications", err));
     }
-    
   }
 
-
-  // {
-  //   "publicKey":"BF7ekuEOKJrtvX4ornpRrZkkv_ALrNVb4r5RzeqzOgZP-oorGGxQsUROVK2oTymCDEkQKlaNb2WVYplrrtp9MtE",
-  //   "privateKey":"M9B2G_q8S6QaN_QwOhshyNa-4tvhm4KzsSrNF51ZNGg"
-  // }
+  sendNotificationRequest() {
+    this.pushNotificationService.addPushSubscriber(this.subscriptionObj).subscribe()
+  }
 }
