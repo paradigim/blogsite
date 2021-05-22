9671222f1104c19c0ed5e337d5e50f4d4615381e
paradigim <pritamdas71.pd@gmail.com>
2021-05-22 12:45:49 +0530

    right side menu implement

--- src/app/shared/right-side-menu/right-side-menu.component.ts
+++ src/app/shared/right-side-menu/right-side-menu.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RightSideMenuComponent implements OnInit {
 
+  totalPosts = Array.from({ length: 3 }).map(() => ({}));
+  totalPosts2 = Array.from({ length: 3 }).map(() => ({}));
+
   constructor() { }
 
   ngOnInit(): void {
